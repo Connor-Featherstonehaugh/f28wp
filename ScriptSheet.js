72712afb5cec087a4f43a331c13abc57990b3e91
Connor-Featherstonehaugh <cf2045@hw.ac.uk>
2023-10-06 13:36:32 +0100

    feat(bAlpha): rmButton tweaks

--- ScriptSheet.js
+++ ScriptSheet.js
@@ -9,18 +9,18 @@ function changeButton() {
     }
   }
 
-  function addRow() {
+  function addRow(button) {
     const table = document.getElementById("dropdownTable").getElementsByTagName('tbody')[0];
-    const newRow = table.insertRow(table.rows.length);
+    const newRow = table.insertRow(button.closest("tr").rowIndex + 1);
     const cell1 = newRow.insertCell(0);
     const cell2 = newRow.insertCell(1);
 
     // Create a new dropdown element
     const dropdown = document.createElement("select");
-    dropdown.innerHTML = document.querySelector('select').innerHTML; // Copy options from the first dropdown
+    dropdown.innerHTML = button.closest("tr").cells[0].querySelector('select').innerHTML; // Copy options from the first dropdown
 
     // Create a button to remove the row
-    cconst removeButton = document.createElement("button");
+    const removeButton = document.createElement("button");
     removeButton.textContent = "Remove";
     removeButton.onclick = function () {
         table.deleteRow(newRow.rowIndex);
