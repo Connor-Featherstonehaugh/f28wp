21a2f4a1040caa0d3c3a502ed39b756a2c31159a
Connor-Featherstonehaugh <cf2045@hw.ac.uk>
2023-10-06 13:31:35 +0100

    feat(branchAlpha):dropdownTable added

--- ScriptSheet.js
+++ ScriptSheet.js
@@ -8,3 +8,24 @@ function changeButton() {
       button.style.backgroundColor = "#007bff";
     }
   }
+
+  function addRow() {
+    const table = document.getElementById("dropdownTable").getElementsByTagName('tbody')[0];
+    const newRow = table.insertRow(table.rows.length);
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+
+    // Create a new dropdown element
+    const dropdown = document.createElement("select");
+    dropdown.innerHTML = document.querySelector('select').innerHTML; // Copy options from the first dropdown
+
+    // Create a button to remove the row
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    removeButton.onclick = function () {
+        table.deleteRow(newRow.rowIndex);
+    };
+
+    cell1.appendChild(dropdown);
+    cell2.appendChild(removeButton);
+}
